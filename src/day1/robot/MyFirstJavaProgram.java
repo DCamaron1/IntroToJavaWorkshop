59acbb129aff9ba84074c65756c561e8184d03cd
DCamaron1 <league@Mac-mini-2.local>
2016-08-15 12:02:25 -0700

    day 1
    yada yadda

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -5,11 +5,23 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
 	public static void main(String[] args) {
-		
-		// START HERE
 
-	
-		
-		
+		// START HERE
+		Robot buddy = new Robot();
+		buddy.setSpeed(100);
+		buddy.penDown();
+		buddy.setPenWidth(20);
+		for (int i = 0; i < 4; i++) {
+			buddy.turn(-90);
+			buddy.move(200);
+		}
+//		 buddy.penUp();
+//		 buddy.turn(-90);
+//		 buddy.move(50);
+//		 buddy.penDown();
+//		 buddy.setPenColor(000, 051, 221);
+//		
+//		 buddy.hide();
+
 	}
 }
